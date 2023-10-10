17c276fa7d8421116a7f7f08dde2a3e995bc8f5a
hairez <harryzhang2004@hotmail.com>
2023-10-11 01:53:35 +0200

    add errorhandling and make variables global

--- code/main.py
+++ code/main.py
@@ -10,11 +10,6 @@ from tkinter import messagebox
 import screeninfo #library to get the width and height of user screen.
 import time
 
-windowWidth=screeninfo.get_monitors()[0].width
-windowHeight=screeninfo.get_monitors()[0].height
-history=[]     #history over the users recent moves
-buttons=[]
-
 class trollButton:
    def __init__(self,currX,currY):
       self.button = tkinter.Button(window, text ="Put troll here!", command = self.disableButton)     
@@ -65,12 +60,12 @@ def finishBoard():
    assert 1
 
 
-def mainGame(n):
+def startGame(n):
 
    #creating 3x3 buttons 
    #TODO make it take in an input n
-   for i in range(1,4):
-      for j in range(1,4):
+   for i in range(1,n+1):
+      for j in range(1,n+1):
          buttons.append(trollButton(150*i,50*j)) #TODO put the buttons between 0 and screenheight-200
          #TODO update the buttons positions if the window changes size
 
@@ -89,9 +84,24 @@ def mainGame(n):
 
 
 def startMenu():
+   global windowWidth, windowHeight, history, buttons
+   windowWidth=screeninfo.get_monitors()[0].width
+   windowHeight=screeninfo.get_monitors()[0].height
+   history=[]     #history over the users recent moves
+   buttons=[]
+
    global window
    window = tkinter.Tk() #initialize window
    window.geometry(f"{windowWidth}x{windowHeight}")
+
+   global textLabel,labelVar
+   labelVar = tkinter.StringVar()
+   labelVar.set("Write an integer larger than 3: ")
+   textLabel = tkinter.Label(window, textvariable=labelVar, height = 5, width = 52)
+   textLabel.place(x=windowWidth//2-400,y=windowHeight//2-200)
+
+
+
    #Creating the textbox for input
    global inputBox
    inputBox = tkinter.Text(window, height = 5, width = 5) 
@@ -102,7 +112,7 @@ def startMenu():
 
    #Input button
    global sizeSelectionButton
-   sizeSelectionButton = tkinter.Button(window, text = "Set Board Size",command = setSize)
+   sizeSelectionButton = tkinter.Button(window, text = "Set Board Size and Start Game",command = setSize)
    sizeSelectionButton.place(x=windowWidth//2,y=windowHeight//2+40) 
    
    #sizeSelectionButton.pack() 
@@ -113,17 +123,31 @@ def startMenu():
    window.mainloop()
 
 
-n=0
+
 def setSize(): #TODO have different funcitons. one for setSize, one for mainMenu and so on
    #TODO move the inputBox creation and sizeSelectionBUtton in this function
    global n
-   n = inputBox.get(1.0, "end-1c") 
+
+   while True:
+      try:
+         n = inputBox.get(1.0, "end-1c")
+         n=int(n)
+         if n>3:
+            break
+         else:
+            labelVar.set("That is not an integer larger than 3! Try again! \n Write an integer larger than 3: ") 
+      
+      except:
+         labelVar.set("That is not an integer. Try again! \n Write an integer larger than 3: ")
+         
    #TODO felhantering av n
 
 
+   textLabel.place_forget()
    inputBox.place_forget()
    sizeSelectionButton.place_forget()
-   mainGame(n)
+
+   startGame(n)
 
 
 startMenu()
