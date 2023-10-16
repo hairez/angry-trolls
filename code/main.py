2ffa6eb4f8a2b5363bc5f37a442a11127b906c57
hairez <harryzhang2004@hotmail.com>
2023-10-17 00:17:54 +0200

    cleaning up the code

--- code/main.py
+++ code/main.py
@@ -8,6 +8,9 @@ from tkinter import messagebox
 #Use this to install:
 #pip install screeninfo
 import screeninfo #library to get the width and height of user screen.
+windowWidth=screeninfo.get_monitors()[0].width
+windowHeight=screeninfo.get_monitors()[0].height
+
 import time
 
 #TODO add comments to everything
@@ -22,13 +25,11 @@ class trollButton:
       self.y = y
 
    def placeTroll(self):
-
       if checkValidMove(self.x, self.y):
          currGrid[self.y][self.x] = 1  #If a troll is placed on a position (x,y), then currGrid[y][x] will be 1, otherwise 0.
          self.button.configure(bg = "green", text = "Undo this move!", command = self.undoMove)
 
          #TODO if a move has been played, check if there are any valid moves left.
-
       else:
          gameLost()
 
@@ -36,13 +37,9 @@ class trollButton:
    def undoMove(self):
       self.button.configure(bg = "gray", text ="Put troll here!", command = self.placeTroll)
       currGrid[self.y][self.x] = 0
-      
-      
-
 
 #Checks if a troll could be placed at position (x,y)
 def checkValidMove(x, y):
-   
    n = len(currGrid)
 
    for i in range(n):
@@ -51,7 +48,6 @@ def checkValidMove(x, y):
       if currGrid[i][x]:#Checks if there is a troll on the same column
          return 0
    
-
    #Check if there is a troll on the \ diagonal
    tempX, tempY = x-min(x,y), y-min(x,y)
    while tempX<n and tempY<n:
@@ -60,7 +56,6 @@ def checkValidMove(x, y):
       tempX+=1
       tempY+=1
    
-
    #Checks if there is a troll on the / diagonal and above (x,y)
    i=1
    while x+i<n and 0<=y-i:
@@ -78,7 +73,6 @@ def checkValidMove(x, y):
    #If no troll has been found, then it is a valid move
    return 1      
 
-
 def openFile():
    #TODO öppnar filen där den sparar alla resultat. prob en CSV fil
    #sparar nuvarande top resultat i en array
@@ -88,7 +82,6 @@ def updateFile():
    #TODO updaterar resultaten i filen efter att ha uppdaterat den med det nya resultatet. 
    #kan lösas med en array
    pass
-
 
 def calculateTime():
    #TODO räknar ut resultatet av spelet, hur lång tid det tar och sparar resultatet i en fil.
@@ -102,54 +95,52 @@ def finishBoard():
    pass
 
 def restartGame():
+   #Hide all buttons form main game
    for button in buttons:
       button.button.grid_forget()
-
-   buttons.clear()
-   currGrid.clear()
-
    finishButton.pack_forget()
-
    restartButton.pack_forget()
-
    leftFrame.pack_forget()
    rightFrame.pack_forget()
 
+   #Clearing up currGrid and buttons so new game could be started
+   buttons.clear()
+   currGrid.clear()
+
+   #Starting the menu
    startMenu()
 
-
 def gameLost():
-
    #A message box displaying that the game is over.
    messagebox.showwarning(title="Game Lost", message="Oh no! The trolls just got angry. Try again! ")
 
+   #Disable all other buttons
    for button in buttons:
       button.button["state"] = tkinter.DISABLED
-
    finishButton["state"] = tkinter.DISABLED
 
+   #Show restartButton
    restartButton.pack(side="right")
 
-   
-
-def startGame(n, windowWidth=screeninfo.get_monitors()[0].width, windowHeight=screeninfo.get_monitors()[0].height):
-   
+def startGame(n):
    #Creating n x n buttons
    for y in range(n):
-      currGrid.append([0]*n)
       for x in range(n):
          buttons.append(trollButton(rightFrame, x, y)) 
    
+   #Creating a grid where all current trolls could be tracked on.
+   for _ in range(n):
+      currGrid.append([0]*n)
+
+   #Add the main left and right frames to the window
    leftFrame.pack(side="left")
    rightFrame.pack(side="right")
 
+   #Add the finishButton
    finishButton.pack(side="top", padx=20, pady=20)
    finishButton["state"]=tkinter.NORMAL
 
-
-
-
-def startMenu(windowWidth=screeninfo.get_monitors()[0].width, windowHeight=screeninfo.get_monitors()[0].height):
+def startMenu():
    #TODO add explanation of how the game works
 
    #Set label-text:
@@ -158,29 +149,16 @@ def startMenu(windowWidth=screeninfo.get_monitors()[0].width, windowHeight=scree
    #Set position of textLabel:
    textLabel.place(x=windowWidth//2-400,y=windowHeight//2-200)
 
-
-
    #Set position for inputBox
    inputBox.place(x=windowWidth//2,y=windowHeight//2-50)
 
-   #grid layout or pack (flow) layout?
-   #inputBox.pack() 
-
    #Set position for sizeSelectionButton
    sizeSelectionButton.place(x=windowWidth//2,y=windowHeight//2+40) 
-   
-   #sizeSelectionButton.pack() 
-
-   
-
-   window.mainloop()
-
-
 
 def setSize(): 
    try:
       n = inputBox.get(1.0, "end-1c")
-      n=int(n)
+      n = int(n)
       if n<4 or n>9:
          labelVar.set("That is not an integer larger than 3 and less than 10! Try again! \n Write an integer larger than 3 and less than 10: ") 
          return #exit the function and the loop until a new value has been entered
@@ -196,11 +174,7 @@ def setSize():
 
    startGame(n)
 
-
-
 window = tkinter.Tk() #Initialize window
-windowWidth=screeninfo.get_monitors()[0].width
-windowHeight=screeninfo.get_monitors()[0].height
 window.geometry(f"{windowWidth}x{windowHeight}")
 
 #Initializing all other labels in the window
@@ -208,21 +182,13 @@ labelVar = tkinter.StringVar()
 textLabel = tkinter.Label(window, textvariable=labelVar, height = 5, width = 52)
 inputBox = tkinter.Text(window, height = 5, width = 5) 
 sizeSelectionButton = tkinter.Button(window, text = "Set Board Size and Start Game",command = setSize)
-
-
+rightFrame=tkinter.Frame(window)
 leftFrame=tkinter.Frame(window)
-
-
 finishButton = tkinter.Button(leftFrame, text = "Finish the board for me", command = finishBoard)
 restartButton = tkinter.Button(leftFrame, text = "Restart Game", command = restartGame)
 
-rightFrame=tkinter.Frame(window)
+buttons=[]     #An array keeping all relevant trollButtons
+currGrid=[]    #A grid tracking the current state of the game
 
-
-buttons=[]
-
-currGrid=[]    #the current game grid
 startMenu()
-
-
-
+window.mainloop()
